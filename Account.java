1940b874e9b502322f041fe6e0ca577f2d72fe61
Trong Nguyen <nguyentrong56@gmail.com>
2021-10-31 20:03:42 -0700

    fixed

--- Account.java
+++ Account.java
@@ -1,5 +1,32 @@
 package main;
 
 public class Account {
+	private AccountHolder holder;
+	private double balance = 0.0;
+	public Account(double amt, AccountHolder holder){
+	this.balance=balance+amt;
+	this.holder = holder;
+	}
+	public double deposit(double amt){
+	return balance = balance+amt;
+	}
+	public boolean withdraw(double amt){
+	if(amt<balance){
+	balance-=amt;
+	return true;
+	}
+	else{
+	return false;
+	}
+	}
+	public AccountHolder getHolder(){
+	return holder;
+	}
+	public void SetBalance(double amt){
+	balance=amt;
+	}
+	public void setHolder(AccountHolder holder){
+	this.holder=holder;
+	}
 
 }
